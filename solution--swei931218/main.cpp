bd93b912b2b1eae16785e49a95a667c4eda2844f
swei931218 <swei931218@gmail.com>
2016-03-26 22:44:42 -0700

    still working on it......

---
+++ solution--swei931218/main.cpp
@@ -0,0 +1,50 @@
+still working on it......
+
+/* ----------------------------------------------------------------------------
+ * Copyright &copy; 2016 Yi SUi <swei931218@csu.fullerton.edu>
+ * Released under the [MIT License] (http://opensource.org/licenses/MIT)
+ * ------------------------------------------------------------------------- */
+
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+using std::endl;
+
+
+void print_vector(const vector<int> &v) {
+    vector<int>::const_iterator x;
+    
+    //outputs the contents
+    for (x=v.begin();xa!= v.end();x++) {
+        cout << *x << " ";
+    }
+    
+    
+    
+    cout << endl;
+}
+
+//selection_sort
+void selection_sort(vector<int> &v) {
+    
+}
+
+//merge sort
+vector<int> merge() {
+    
+}
+
+
+vector<int> merge_sort(const vector<int> &m) {
+    
+    
+}
+
+
+int main() {
+   
+    return 0;
+}
+
+
